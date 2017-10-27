2c5a30f1935e932e736955156fb06b202d017ab3
Servio Paladines <spcompu@gmail.com>
2017-10-27 09:35:17 -0500

    Update panprimo.py

--- panprimo.py
+++ panprimo.py
@@ -1,29 +1,32 @@
 def panprimo(n):
-	numstr = str(n)
-	cero,uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve = 0,0,0,0,0,0,0,0,0,0
-	primo = int(numstr[-3:])
-	for i in numstr:
-		if i == "0":
-			cero = 1
-		if i == "1":
-			uno = 1
-		if i == "2":
-			dos = 1
-		if i == "3":
-			tres = 1
-		if i == "4":
-			cuatro = 1
-		if i == "5":
-			cinco = 1
-		if i == "6":
-			seis = 1
-		if i == "7":
-			siete = 1
-		if i == "8":
-			ocho = 1
-		if i == "9":
-			nueve = 1
-	if cero == 1 and uno == 1 and dos == 1 and tres == 1 and cuatro == 1 and cinco == 1 and seis == 1 and siete == 1 and ocho == 1 and nueve == 1 and primo%2 != 0:
-		return True
-	else:
-		return False
+  numstr = str(n)
+  cero,uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve = 0,0,0,0,0,0,0,0,0,0
+  primo = int(numstr[-3:])
+  for i in numstr:
+    if i == "0":
+      cero = 1
+    if i == "1":
+      uno = 1
+    if i == "2":
+      dos = 1
+    if i == "3":
+      tres = 1
+    if i == "4":
+      cuatro = 1
+    if i == "5":
+      cinco = 1
+    if i == "6":
+      seis = 1
+    if i == "7":
+      siete = 1
+    if i == "8":
+      ocho = 1
+    if i == "9":
+      nueve = 1
+  if cero == 1 and uno == 1 and dos == 1 and tres == 1 and cuatro == 1 and cinco == 1 and seis == 1 and siete == 1 and ocho == 1 and nueve == 1:
+    for i in range(2,primo):
+      if (primo%i)==0:
+        return False
+    return True
+  else:
+    return False
